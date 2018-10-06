9f9b5c25382c956f4d04dece8e1de370177a0523
koshys <samson@samsonkoshy.com>
2018-10-06 16:40:58 -0700

    start working on the game obj

---
+++ assets/javascript/game.js
@@ -0,0 +1,22 @@
+/**
+ * Word guessing Game
+ */
+
+
+ /**
+  * main game object that contains 
+  * 
+  * . the list of possible words
+  * . the number of words guessed
+  * . remaining words guessed
+  * 
+  */
+ var wordGameObj = {
+
+    /**
+     * data structure
+     */
+
+
+
+ }
