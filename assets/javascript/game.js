f3b6ed7035d1c8ec3e422813206a2ee67bf204cb
koshys <samson@samsonkoshy.com>
2018-10-06 17:59:02 -0700

    get text on the screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,12 +11,62 @@
   * . remaining words guessed
   * 
   */
- var wordGameObj = {
+ function WordGameObj() {
 
     /**
      * data structure
+     * 
+     * list of possible words
      */
+    this.dict = [
+        "madonna",
+        "terminator",
+        "genesis"
+    ],
+    this.static = null,
+    this.winsStatic = null,
+    this.wins = 0,
+    this.choosenWordStatic = null,    
+    this.choosenWord = [],
+    this.currentWordStatic = null,    
+    this.currentWord = [],
+    this.guessedLettersStatic = null,        
+    this.guessedLetters = [],
+    this.totalGuessesStatic = null,            
+    this.totalGuesses = 0,
 
+    /**
+     * set statics
+     */
+    this.setStatic = function() {
+        this.static = "Press any Key to Continue..";
+        this.winsStatic = "Wins";
+    }
+
+    /**
+     * get a random word for the game
+     * max = (this.dict.length -1)
+     * min = 0
+     * 1 is necessary when using floor to get an equal distribution of values called.
+     */
+    this.setChoosenWord = function() {
+
+        // ramdom * ( max - min ) + min 
+        this.choosenWord = this.dict[Math.floor(Math.random() * ( 1+ (this.dict.length -1) - 0  ) ) + 0 ].split("");
+
+    }
 
 
  }
+
+  var w = new WordGameObj();
+  w.setStatic();
+  document.querySelector(".static").innerHTML = w.static;
+  document.querySelector(".wins-static").innerHTML = w.winsStatic;
+  document.querySelector(".wins").innerHTML = w.wins;
+
+  w.setChoosenWord();
+
+  console.log(w.choosenWord.join(""));
+
+  
